37ae22cfdd60676fa6d0bc8584a9687a9e8bef8c
biker <ujschenlin@gmail.com>
2017-09-20 16:51:25 +0800

    bug

--- MpWechat.php
+++ MpWechat.php
@@ -1283,19 +1283,18 @@ class MpWechat extends BaseWechat
      * @param string $action
      * get_confirm
      * @param string $scene 重定向后会带上scene参数，开发者可以填0-10000的整形值，用来标识订阅场景值
-     * @param string $template_id 订阅消息模板ID，登录公众平台后台，在接口权限列表处可查看订阅模板ID
      * @param string $reserved 用于保持请求和回调的状态，授权请后原样带回给第三方。该参数可用于防止csrf攻击（跨站请求伪造攻击），
      * 建议第三方带上该参数，可设置为简单的随机数加session进行校验，开发者可以填写a-zA-Z0-9的参数值，最多128字节
      * @return string
      */
-    public function getSubcribeMsgUrl($redirectUrl, $scene  , $template_id ,  $reserved = 'biker')
+    public function getSubcribeMsgUrl($redirectUrl, $scene  , $reserved = 'biker')
     {
         return $this->httpBuildQuery(self::WECHAT_SUBSCRIBE_MSG_URL, [
                 'appid' => $this->appId,
                 'action' => 'get_confirm',
                 'scene' =>  $scene,
-                'template_id'   =>  $template_id,
-                'redirect_uri' => $redirectUrl,
+                'template_id'   =>  $this->template_id,
+                'redirect_url' => $redirectUrl,
                 'reserved'  =>  $reserved
             ]) . '#wechat_redirect';
     }
